4f94dd700ea466f01a4fcdfe8f8b64266a3429c1
Kelly Costa <kellydosocorro@gmail.com>
2019-04-04 23:15:47 -0300

    Bresenham
    TODO: Interface gráfica

--- Computação Gráfica/Bresenham/Bresenham.py
+++ Computação Gráfica/Bresenham/Bresenham.py
@@ -1,3 +1,5 @@
+# Bresenham 
+
 def calculo_m(x1,x2,y1,y2):
     deltaY = y2-y1
     deltaX = x2-x1
@@ -7,75 +9,100 @@ def calculo_m(x1,x2,y1,y2):
     else:
         return deltaY
 
-
 def ponto(ponto):
     x,y = [int(p) for p in ponto.split(',')]
     return x,y
 
-def reflexao(p1,p2):
+def reflection(m,p1,p2):
 
-    result = []
-    trocaXY, trocaX, trocaY = False, False, False
+    troca = []
 
     x1,y1 = ponto(p1)
     x2,y2 = ponto(p2)
-
-    m = calculo_m(x1,x2,y1,y2)
-    result.append(m)
-
-    if m >1 or m < -1:
+    
+    if m>1 or m< -1:
         x1,y1 = y1,x1
-        x2, y2 = y2, x2
-        trocaXY = True
-        #result.append(trocaXY)
-        result.append(x1)
-        result.append(y1)
-        result.append(x2)
-        result.append(y2)
+        x2,y2 = y2,x2
+        troca.append(True)
+    else:
+        troca.append(False)
 
     if x1 > x2:
         x1 = -x1
         x2 = -x2
-        trocaX = True
-        #result.append(trocaX)
-        result.append(x1)
-        result.append(x2)
+        troca.append(True)
+    else:
+        troca.append(False)
 
     if y1 > y2:
         y1 = -y1
         y2 = -y2
-        trocaY = True
-        #result.append(trocaY)
-        result.append(y1)
-        result.append(y2)
-		
-    #return result
-    return result
-	
+        troca.append(True)
+    else:
+        troca.append(False)
+        
+    return [[x1,y1],[x2,y2], troca]
+
+def negativa(lista):
+    for i in range(len(lista)):
+        lista[i] = -lista[i]
+    return lista
+
+def Bresenham(p1,p2):
+
+    x1,y1 = ponto(p1)
+    x2,y2 = ponto(p2)
+
+    m = 0
+    e = 0
+
+    m = calculo_m(x1,x2,y1,y2)
+    
+    resultado = reflection(m,p1,p2)
+
+    X1,Y1 = resultado[0]
+    X2,Y2 = resultado[1]
+
+    m = calculo_m(X1,X2,Y1,Y2)
+    e = m - 0.5
+
+    pontosX = []
+    pontosY = []
+
+    pontosX.append(X1)
+    pontosY.append(Y1)
+
+    while(X1 < X2):
+        if (e >= 0):
+            Y1 = Y1+ 1
+            e = e - 1  
+        X1 = X1+1
+        e = m + e
+        pontosX.append(X1)
+        pontosY.append(Y1)
+
+    # Inverte a ordem
+    if resultado[2][0] == True:
+        pontosX, pontosY = pontosY, pontosX
+    if resultado[2][1] == True:
+        pontosX = [-p for p in pontosX]
+    if resultado[2][2] == True:
+        pontosY = [-p for p in pontosY]
+    
+    print(pontosX)
+    print(pontosY)
+
+
+# Escopo principal
+
 def main():
-		
+
     ponto1 = input("Digite os valores para o ponto 1 (x,y): ")
     ponto2 = input("Digite os valores para o ponto 2 (x,y): ")
 
-    resultado = reflexao(ponto1, ponto2)
-	
-    x1,y1 = resultado[1], resultado[2]
-    x2,y2 = resultado[3], resultado[4]
-
-    e = 0
-    m = resultado[0]
-
-    while x1 < x2:
-        if e>=0:
-            y1 = y1+1
-            e=e-1
-        x1 = x1+1
-        e = e+m
-		
-        print("e = {}".format(e))
-        print("m = {}".format(m))
-        print("Ponto ({},{})".format(x1,y1))
+    Bresenham(ponto1,ponto2)
 
 main()
 
+    
 
